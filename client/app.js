e934d0b08c73217c0190aa9183f1433ac1372877
Lukasz Jachym <lukasz.jachym@gmail.com>
2015-05-26 16:17:59 +0200

    adds: debug info turned off by default and available only for local environment

--- client/app.js
+++ client/app.js
@@ -1,16 +1,23 @@
 'use strict';
 
 angular.module('zengSeed', [
-  'ngRoute',
-  'ngCookies'
-])
-  .config(function ($routeProvider, $locationProvider) {
+        'ngRoute',
+        'ngCookies'
+    ])
+    .constant('appConfig', {
+        debugMode: false
+    })
+    .config(($compileProvider, $logProvider, appConfig) => {
+        $compileProvider.debugInfoEnabled(appConfig.debugMode);
+        $logProvider.debugEnabled(appConfig.debugMode);
+    })
+    .config(function ($routeProvider, $locationProvider) {
 
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
+        $routeProvider
+            .otherwise({
+                redirectTo: '/'
+            });
 
-    $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-  });
+    });
